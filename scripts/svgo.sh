ebb0e30d9ae0658af2234349d1947cf0d0941e41
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2025-10-22 13:58:11 +0200

    Updated themes

--- scripts/svgo.sh
+++ scripts/svgo.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-IMAGES=$(find "Source Files" -name '*.svg') ./themes/projektemacher-base/scripts/svgo.sh static/images
+IMAGES=$(find "Source Files" -name '*.svg')
+
+if [ -n "$IMAGES" ] ; then
+  IMAGES=$IMAGES ./themes/projektemacher-base/scripts/svgo.sh static/images
+else
+  echo "No SVG Files found!"
+fi
