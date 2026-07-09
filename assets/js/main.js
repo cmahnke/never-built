ee54c4b7745b102dd160895e05fb16a350955a61
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-07-10 00:30:58 +0200

    Cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,6 @@ import {setupAnimatedLinks, setupAnimatedMenu} from './animated-link';
 import {setupBook} from './scrollify';
 import Glide from '@glidejs/glide'
 import {imageViewer} from './image-viewer';
-//import { projektemacherMap } from './maps/projektemacher-map';
-//import {Style, Fill, Stroke, Icon} from 'ol/style.js';
 import {detect} from 'detect-browser';
 
 
@@ -22,53 +20,6 @@ if (browser && (browser.name === 'ios' || browser.name === 'ios-webview')) {
   });
 }
 
-
-/*
-window.projektemacherMap = async function(elem, geojson, source, style, bbox, center, initialZoom, minZoom, maxZoom, cluster, disabled, popup, background, debug, marker, font) {
-  var bgElem;
-  if (typeof elem === "string") {
-    bgElem = document.getElementById(elem)
-  }
-  if (font === undefined) {
-    font = defaultMapFont;
-  }
-  if (!(typeof marker === 'object')) {
-    marker = JSON.parse(marker)
-  }
-  function createStyleFunction(marker) {
-    return (feature, level) => {
-      const lineWidth = Math.floor(50 / level)
-      return [new Style({
-          image: new Icon(marker)
-        }),
-        new Style({
-          stroke: new Stroke({
-            color: 'rgba(0,0,0,1)',
-            width: lineWidth + 4
-          }),
-        }),
-        new Style({
-          stroke: new Stroke({
-            color: 'rgba(255,255,255,1)',
-            width: lineWidth
-          })
-        })]
-    };
-  }
-  background = window.getComputedStyle(bgElem).getPropertyValue('--page-background');
-  const map = projektemacherMap(elem, geojson, source, style, bbox, center, initialZoom, minZoom, maxZoom, cluster, disabled, popup, background, debug, createStyleFunction(marker), font);
-
-  if (!("projektemacher" in window)) {
-    window.projektemacher = {};
-  }
-  if (!("maps" in window.projektemacher)) {
-    window.projektemacher.maps = {};
-  }
-  window.projektemacher.maps[bgElem] = await map
-
-  return map
-}
-*/
 
 window.anchorTop = (anchor) => {
   const heading = document.querySelector(`.header-background`)
